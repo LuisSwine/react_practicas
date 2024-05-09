9cf8434471ff5c651e787093fba65d8efb0dcade
Luis Angel Lopez Alvarez <luis.luis.la2002@gmail.com>
2024-05-09 16:53:15 -0600

    Nuevas etapas del curso

---
+++ gif-expert-app/src/GifExpertApp.jsx
@@ -0,0 +1,13 @@
+const GifExpertApp =() => {
+
+    return (
+        {/*Titulo*/}
+        <h1>GifExpertApp</h1>
+
+        {/*Input*/}
+
+        {/*Listado*/}
+    );
+};
+
+export default GifExpertApp;
